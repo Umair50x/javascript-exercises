668868ae7a6dc8dcde2ed790957cdee921bc414b
Umair50x <i2u1@outlook.com>
2023-03-26 16:53:54 +0500

    Add for() to eval and append the string to a new var which is returned.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function( string, num ) {
+
+    if(num < 0) return "ERROR";
+    let word = '';
+
+    for (let i = 0; i < num; i++) {
+        word += string;
+    }
+
+    return word;
 
 };
 
